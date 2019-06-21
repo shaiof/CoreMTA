47a1b2b1f98ad77efb5fb562d06cb3ac2a212d5c
moon91210 <smulders@protonmail.com>
2019-06-21 22:19:27 +0200

    update test resource

--- addons/test3/client.lua
+++ addons/test3/client.lua
@@ -1,1 +1,9 @@
-Ped(22, Player('tails').position)
+print('the client')
+
+
+local p = Ped(32, Player('morty').position)
+local p2 = Ped(35, Player('morty').position)
+
+addEventHandler('onClientPedWasted', resourceRoot, function()
+    iprint('clientwasted', source or 'nada', source.model)
+end)
---
+++ addons/test3/server.lua
@@ -0,0 +1,6 @@
+local p = Ped(22, Player('morty').position)
+local p2 = Ped(25, Player('morty').position)
+
+-- addEventHandler('onPedWasted', resourceRoot, function()
+--     iprint('wasted', source or 'nada', source.model)
+-- end)
